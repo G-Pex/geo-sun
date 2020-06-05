c3962c481d15c3ff1dc66ec3171c74d3d2191b57
Gareth Perry <garethnperry@gmail.com>
2020-06-05 08:10:56 +0100

    create reusable icon and input components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import InputWithIcon from 'components/global/inputWithIcon';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  position: relative;
+  height: 100%;
+  width: 100%;
+`;
+const Header = styled.div`
+  position: relative;
+  height: 100px;
+  width: 100%;
+  border-bottom: 1px solid black;
+`;
+
+const InputBoxes = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  height: calc(100% - 100px);
+  width: 100%;
+  border: 1px solid black;
+  box-sizing: border-box;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Header></Header>
+      <InputBoxes>
+        <InputWithIcon
+          {...{
+            height: 60,
+            width: 400,
+            placeholderText: 'Please enter your location',
+          }}
+        />
+        <InputWithIcon
+          {...{
+            height: 60,
+            width: 400,
+            placeholderText: 'Sunrise',
+          }}
+        />
+        <InputWithIcon
+          {...{
+            height: 60,
+            width: 400,
+            placeholderText: 'Sunset',
+          }}
+        />
+      </InputBoxes>
+    </AppContainer>
   );
 }
 
---
+++ src/components/global/interactiveIcon.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const IconSvg = styled.svg`
+  position: relative;
+
+  height: ${props => props.height}px;
+  width: ${props => props.width}px;
+
+  margin-left: ${props => props.margin.left};
+  margin-right: ${props => props.margin.right};
+
+  padding-top: ${props => props.padding.top}px;
+  padding-bottom: ${props => props.padding.bottom}px;
+  padding-left: ${props => props.padding.left}px;
+  padding-right: ${props => props.padding.right}px;
+
+  :hover {
+    height: ${props =>
+      props.hoverHeight ? props.hoverHeight : props.height}px;
+    width: ${props => (props.hoverWidth ? props.hoverWidth : props.width)}px;
+  }
+`;
+
+export default function InteractiveIcon(props) {
+  const {
+    height,
+    width,
+    hoverHeight,
+    hoverWidth,
+    svg,
+    svgClass,
+    fillColour,
+    lineColour,
+    margins,
+    paddings,
+  } = props;
+
+  // If margins not supplied use auto left/right
+  let margin = margins || { left: 'auto', right: 'auto' };
+
+  //If paddings not supplied default all to 0
+  let padding = paddings || { top: 0, bottom: 0, left: 0, right: 0 };
+  return (
+    <IconSvg
+      height={height ? height : 30}
+      width={width ? width : 30}
+      hoverHeight={hoverHeight}
+      hoverWidth={hoverWidth}
+      margin={margin}
+      padding={padding}
+    >
+      <use xlinkHref={svg + svgClass} fill={fillColour} line={lineColour} />
+    </IconSvg>
+  );
+}
+
+InteractiveIcon.propTypes = ({ object, string, number } = PropTypes) => {
+  return {
+    /* The SVG graphic used by the icon*/
+    svg: object,
+    /* The SVG class used to override styles*/
+    svgClass: string,
+    /* The colour used to override the style 'fill' */
+    fillColour: string,
+    /* The colour used to override the style 'line' */
+    lineColour: string,
+    /* The margins of the icon */
+    margins: object,
+    /* The paddings of the icon */
+    paddings: object,
+    /* The height in px of the icon */
+    height: number,
+    /* The width in px of the icon */
+    width: number,
+    /* The height in px of the icon when mouse hovers over */
+    hoverHeight: number,
+    /* The width in px of the icon when mouse hovers over */
+    hoverWidth: number,
+  };
+};
